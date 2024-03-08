7cd8f133e21b9e286624c5c12e08d07fbb560812
JaneDoe <e.janedoe.87@gmail.com>
2024-03-08 11:08:11 +0300

    task1 added

--- Recursion/task1/Program.cs
+++ Recursion/task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные 
+//числа в промежутке от M до N.
+//Использовать рекурсию, не использовать циклы.
+
+int m = 8;
+int n = 100;
+
+void PrintNumbers(int min, int max){
+    if (max < min){
+        return;
+    }
+    
+    PrintNumbers(min, max - 1);
+    System.Console.Write($"{max} ");
+}
+
+PrintNumbers(m, n);
